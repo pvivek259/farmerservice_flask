ece051b6c008839844d60995e94eb09f78ea407a
Vivek Pandey <vivek.pandey@agrostar.in>
2021-06-23 12:29:08 +0530

    remove the file

--- farmerservice/views/shippingController.py
+++ farmerservice/views/shippingController.py
@@ -26,13 +26,13 @@ def add_shipping_address(request_param):
     farmer_obj = Farmer.objects.get(phone_no = phone_number)
     #create  shipping address object, and add farmer ID
     shipping_address_object = ShippingAddress.objects.create(farmer = farmer_obj)
-
-    shipping_address_object.name = request_param.get("name", None),
-    shipping_address_object.address1 = request_param.get("address1", None),
-    shipping_address_object.address2 =request_param.get("address2", None),
-    shipping_address_object.pin_code = request_param.get("pin_code", None),
-    shipping_address_object.village = request_param.get("village", None),
-    shipping_address_object.city =request_param.get("city", None),
+    print(type(request_param))
+    shipping_address_object.name = request_param.get("name", None)
+    shipping_address_object.address1 = request_param.get("address1", None)
+    shipping_address_object.address2 =request_param.get("address2", None)
+    shipping_address_object.pin_code = request_param.get("pin_code", None)
+    shipping_address_object.village = request_param.get("village", None)
+    shipping_address_object.city =request_param.get("city", None)
     shipping_address_object.country =request_param.get("country", None)
 
     #save shipping address object
@@ -69,7 +69,8 @@ def parse_shipping_addresses(shipping_address_objs):
 
 def parse_shipping_address(shipping_address):
     shipping_address_json = {}
-    shipping_address_json[id] = shipping_address.id
+    print(shipping_address.id)
+    shipping_address_json["id"] = shipping_address.id
     shipping_address_json["name"] = shipping_address.name
     shipping_address_json["address1"] = shipping_address.address1
     shipping_address_json["address2"] = shipping_address.address2
